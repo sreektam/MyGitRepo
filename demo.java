6ac25ef83cc52eb686276c21c71f9f687d443037
Sreekta <sreektam@gmail.com>
2023-02-21 17:12:17 +0000

    ignore files

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class Demo {
 	public static void main(String arg[]){
 		//Print statement for Hello World
-		System.out.println("Hello World");
+		System.out.println("Hello World This is Sreekta");
 
 	}
 }
